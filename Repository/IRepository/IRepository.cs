89d9d6ad7f3d2a2ff7bdf0c8e7aab7789018764f
Jeremiah Martinez <itsjeremiahmartinez@gmail.com>
2022-09-10 17:02:27 +1200

    Created Repository Layer for Onion Structure
    - Altered backend to create Repository Layer project to follow the onion structure

---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+
+namespace Repository.IRepository
+{
+    public interface IRepository <T> where T : AlbumReview
+    {
+        IEnumerable<T> GetAll ();
+        T Get (int Id);
+        void Insert(T albumReview);
+        void Update (T albumReview);
+        void Delete (T albumReview);
+        void Remove (T albumReview);
+        void SaveChanges();
+    }
+}
---
+++ Repository/Repository/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Domain.Data;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.IRepository;
+
+namespace Repository.Repository
+{
+    public class Repository <T> : IRepository <T> where T : AlbumReview
+    {
+        private readonly DatabaseContext dbContext;
+        private DbSet<T> albumReviews;
+
+        public Repository(DatabaseContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.albumReviews = this.dbContext.Set<T>();
+        }
+
+        public void Delete(T albumReview)
+        {
+            if (albumReview == null)
+            {
+                throw new ArgumentNullException("album review");
+            }
+            albumReviews.Remove(albumReview);
+            dbContext.SaveChanges();
+        }
+
+        public T Get(int Id)
+        {
+            return albumReviews.SingleOrDefault(c => c.Id.Equals(Id))!;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return albumReviews.AsEnumerable();
+        }
+
+        public void Insert(T albumReview)
+        {
+            if (albumReview == null)
+            {
+                throw new ArgumentNullException("album review");
+            }
+            albumReviews.Add(albumReview);
+            dbContext.SaveChanges();
+        }
+
+        public void Remove(T albumReview)
+        {
+            if (albumReview == null)
+            {
+                throw new ArgumentNullException("album review");
+            }
+            albumReviews.Remove(albumReview);
+        }
+
+        public void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public void Update(T albumReview)
+        {
+            if (albumReview == null)
+            {
+                throw new ArgumentNullException("album review");
+            }
+            albumReviews.Update(albumReview);
+            dbContext.SaveChanges();
+        }
+    }
+}
